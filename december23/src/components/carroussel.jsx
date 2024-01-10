bd652e96e993bc8a0448dd9ec0b8f4b032d61d25
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2024-01-09 15:06:21 -1200

    new feature on the carrousel

--- december23/src/components/carroussel.jsx
+++ december23/src/components/carroussel.jsx
@@ -1,8 +1,10 @@
 import citylights from './citylights1.jpg'
-import {icons} from 'react-icons/bs'
+import {BsChevronCompactLeft,BsChevronCompactRight} from 'react-icons/bs'
+import {RxDotFilled} from 'react-icons/rx'
 import React1 from './react1.jpg'
 import reatct2 from './react2.jpg'
-import nailart from './nail art.jpg'
+import nailart from './nailArt.jpg'
+import { useState } from 'react'
 
 
 export default function Carroussel(){
@@ -20,9 +22,42 @@ export default function Carroussel(){
             img: nailart,
         }
     ]
+    const [currentImg,setCurrentImg]= useState(0)
+function forward(){
+    if(currentImg < 3){
+        setCurrentImg((oldValue)=> oldValue + 1 )
+    }
+    
+}
+function backward(){
+    if (currentImg > 0 ){
+    setCurrentImg((oldValue)=> oldValue - 1 )
+    }
+}
+ function goToSlide(slideIndex){
+    setCurrentImg(slideIndex);
+ }
+
     return(
-        <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
-            <div style={{backgroundImage:`url(${slides[0].img})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'></div>
+        <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+            <div style={{backgroundImage:`url(${slides[currentImg].img})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
+                <div onClick={backward} className=' hidden group-hover:block absolute top-[50] translate-x-0 translate-y-[-50] left-5 mt-16 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <BsChevronCompactLeft size={30}/>
+                </div>
+                <div onClick={forward} className=' hidden group-hover:block absolute top-[50] translate-x-0 translate-y-[-50] right-5 mt-16 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <BsChevronCompactRight size={30}/>
+                </div>
+
+            </div>
+            <div className='flex top-4 justify-center py-2'>
+                {slides.map((slide,slideIndex)=>{
+                    return(
+                        <div key={slideIndex} onClick={()=>goToSlide(slideIndex)} className='text-2xl cursor-pointer'>
+                            <RxDotFilled/>
+                            </div>
+                    )
+                })}
+            </div>  
         </div>
     )
 }
